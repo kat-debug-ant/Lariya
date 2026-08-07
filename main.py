2217650e34992019cde253fdf80ed4353f14c786
kat-debug-ant <arttms06@gmail.com>
2026-08-07 21:04:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(

2cedf105597915724530ce791d6a240394226f44
kat-debug-ant <arttms06@gmail.com>
2026-08-07 16:59:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+print("Token nalezen:", TOKEN is not None)
 
 intents = discord.Intents.default()
 intents.message_content = True
